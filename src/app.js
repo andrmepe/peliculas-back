aea9a43ef3b3af42c21fb778d8f9e1fc1fb889dc
Andres Mejia <anmepe.sud@gmail.com>
2022-12-15 11:46:17 +1030

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import {BD} from './db.js'
+import {PORT} from './config.js'
+
+const app = express()
+
+app.get('/', async(req, res)=>{
+    const rows = await BD.query('SELECT * from peliculas')
+    res.json(rows)
+})
+
+app.listen(PORT)
+console.log('server port: 3001')

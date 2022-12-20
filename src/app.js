db58b17008ba188b514b8e3d71a8a196768a3e6f
Andres Mejia <anmepe.sud@gmail.com>
2022-12-21 09:34:04 +1030

    instalation dotenv

--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@ import express from 'express'
 import {BD} from './db.js'
 import {PORT} from './config.js'
 
+
 const app = express()
 
 app.get('/', async(req, res)=>{
     const rows = await BD.query('SELECT * from peliculas')
     res.json(rows)
+})
+
+app.get('/crearPelicula', async (req, res)=>{
+    const resultado = await BD.query('INSERT INTO peliculas(nombre) VALUES("Guerra de las galaxias")')
+    res.json(resultado)
 })
 
 app.listen(PORT)
